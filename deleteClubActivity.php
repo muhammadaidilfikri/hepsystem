58d2e783ae2e041a3de9b68f35ff8add08309793
Shahh1710git config --global user.email <shahmarzuki664@gmail.com>
2025-10-17 16:46:59 +0800

    updated soft delete

--- deleteClubActivity.php
+++ deleteClubActivity.php
@@ -2,10 +2,25 @@
 session_start();
 include ("dbconnect.php");
 
-$act_id = $_GET["act_id"];
+//$act_id = $_GET["act_id"];
 
-$sql = "delete from club_activities  where act_id='$act_id'";
-$mysqli->query($sql);
+$token = filter_input(INPUT_GET, "act_id", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$isactive = 0;
+$timestamp = date("Y-m-d H:i:s");
+
+// $sql = "delete from club_activities  where act_id='$act_id'";
+
+$sql = "UPDATE club_activities SET isactive = ?, deleted_at = ? WHERE token = ?";
+//$mysqli->query($sql);
+
+//$query = "select * from club_activities where act_id=? ";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("iss", $isactive, $timestamp, $token);
+$stmt->execute();
+//$result = $stmt->get_result();
+
+
+
 
 echo "<script>
 			alert('Acitivity Sucessfully Deleted.');
