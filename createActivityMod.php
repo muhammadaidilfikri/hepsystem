00481ec513a625f2b9de777923b536bb1935be68
Shahh1710git config --global user.email <shahmarzuki664@gmail.com>
2025-10-17 16:43:21 +0800

    update add token and soft delete

--- createActivityMod.php
+++ createActivityMod.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 include ("dbconnect.php");
+include ("iqfunction.php");
 
+// Get form data
+$token = generateToken(32);
 $act_name = $_POST['act_name'];
 $date_start = $_POST['date_start'];
 $date_end = $_POST['date_end'];
@@ -15,15 +18,31 @@ $addedBy = $_SESSION["username"];
 $date_added = date("Y/m/d H:i:s");
 $club_stat = $_POST['club_stat'];
 $kew_id = $_POST['kew_id'];
-$kod_sem = $_POST['kod_sem']; // CHANGED FROM 'semester' to 'kod_sem'
+$kod_sem = $_POST['kod_sem'];
 
-$sql2 = "INSERT INTO club_activities (club_id, act_name, date_start, date_end, location, level_id, total_pax, budget, club_stat, act_allow, addedBy, date_added, kew_id, kod_sem) 
-         VALUES ('$club_id', '$act_name','$date_start','$date_end','$location','$level_id','$total_pax','$budget','$club_stat','$act_allow','$addedBy','$date_added','$kew_id', '$kod_sem')";
 
-$mysqli->query($sql2) or die($mysqli -> error);
+// Use prepared statement to prevent SQL injection
+$sql2 = "INSERT INTO club_activities (club_id, act_name, date_start, date_end, location, level_id, total_pax, budget, club_stat, act_allow, addedBy, date_added, kew_id, kod_sem, token) 
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-echo "<script>
-    alert('Activity Successfully created.');
-    document.location = 'clubActivities.php';
-</script>";
+$stmt = $mysqli->prepare($sql2);
+$stmt->bind_param("issssiidssssiis", 
+    $club_id, $act_name, $date_start, $date_end, $location, 
+    $level_id, $total_pax, $budget, $club_stat, $act_allow, 
+    $addedBy, $date_added, $kew_id, $kod_sem, $token
+);
+
+if ($stmt->execute()) {
+    echo "<script>
+        alert('Activity Successfully created.');
+        document.location = 'clubActivities.php';
+    </script>";
+} else {
+    echo "<script>
+        alert('Error creating activity: " . addslashes($stmt->error) . "');
+        history.back();
+    </script>";
+}
+
+$stmt->close();
 ?>
--- updateActivityMod.php
+++ updateActivityMod.php
@@ -1,43 +1,44 @@
 <?php
 session_start();
-include ("dbconnect.php");
+include("dbconnect.php");
+include("iqfunction.php");
 
 $act_id = $_POST['act_id'];
 $act_name = $_POST['act_name'];
 $date_start = $_POST['date_start'];
-$date_end= $_POST['date_end'];
+$date_end = $_POST['date_end'];
 $location = $_POST['location'];
 $level_id = $_POST['level_id'];
 $budget = $_POST['budget'];
 $club_stat = $_POST['club_stat'];
 $total_pax = $_POST['total_pax'];
 $act_allow = $_POST['act_allow'];
+$token = generateToken(32);
 $addedBy = $_SESSION["username"];
 $kew_id = $_POST["kew_id"];
 $date_added = date("Y/m/d H:i:s");
 
+$query = "select * from club_activities where act_id=? ";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("i", $act_id);
+$stmt->execute();
+$result = $stmt->get_result();
 
+if ($result->num_rows == 0) {
+    echo "<script>
+            alert('Error!! Activity not found');
+            document.location.href = 'clubActivities.php'
+         </script>";
+}
+else {
+    $sql = "update club_activities set kew_id=?, total_pax=?, act_name=?, date_start=?, date_end=?, location=?, level_id=?, budget=?, act_allow=?, club_stat=?, date_added=?, addedBy=?, token=? where act_id=?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("iissssidsssssi", $kew_id, $total_pax, $act_name, $date_start, $date_end, $location, $level_id, $budget, $act_allow, $club_stat, $date_added, $addedBy, $token, $act_id);
+    $stmt->execute();
 
-//echo $act_id."<br>";
-//echo $act_name."<br>";
-//echo $date_start."<br>";
-//echo $date_end."<br>";
-//echo $location."<br>";
-//echo $level_id."<br>";
-//echo $budget."<br>";
-//echo $total_pax."<br>";
-//echo $act_allow."<br>";
-//echo $addedBy."<br>";
-//echo $date_added."<br>";
-//echo $club_stat."<br>";
-
-
-$sql = "update club_activities set kew_id='$kew_id', total_pax='$total_pax', act_name='$act_name', date_start='$date_start',date_end='$date_end', location='$location', level_id='$level_id', budget='$budget',act_allow='$act_allow', club_stat='$club_stat', date_added='$date_added',addedBy='$addedBy' where act_id='$act_id' ";
-$mysqli->query($sql);
-
-
-echo "<script>
-			alert('Activity Sucessfully Updated.');
-			document.location = 'clubActivities.php';
-		 </script>";
+    echo "<script>
+            alert('Activity Successfully Updated.');
+            document.location.href = 'clubActivities.php'
+         </script>";
+}
 ?>
