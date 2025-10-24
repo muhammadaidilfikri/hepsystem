6843c7c8e8e3c35aa083001722561d787146dd23
adlfkry <aidil051110@gmail.com>
2025-10-24 17:15:06 +0800

    update <?php on studList.php(FIKRY)

--- studList.php
+++ studList.php
@@ -8,16 +8,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-// Validate and sanitize club_id
-//$sid = isset($_GET["club_id"]) ? intval($_GET["club_id"]) : 0;
-//if($sid <= 0) {
-//    die("Invalid club ID");
-//}
-$sid = filter_input(INPUT_GET, 'club_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$sid = $_GET["club_id"];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="en" >
 	<!-- begin::Head -->
 	<head>
 		<meta charset="utf-8" />
@@ -71,18 +67,17 @@ $sid = filter_input(INPUT_GET, 'club_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 					<div class="col-lg-12">
             <?php
-						$sql_events2 = mysqli_query($connection, "SELECT * FROM club WHERE token='$sid'") or die(mysqli_error($connection));
-						if(mysqli_num_rows($sql_events2) > 0) {
-							$row = mysqli_fetch_array($sql_events2);
+						$sql_events2 = mysqli_query($connection, "select * from club where club_id='$sid' ") or die (mysqli_error());
+						while ($row = mysqli_fetch_array($sql_events2)) {
+
 							$club_id = $row["club_id"];
 							$club_name = $row["club_name"];
 							$club_max = $row["club_max"];
 							$club_stat = $row["club_stat"];
 							$club_obj = $row["club_obj"];
-						} else {
-							die("Club not found");
-						}
-						?>
+
+								}
+							?>
 
 						<div class="m-portlet m-portlet--tab">
 							<div class="m-portlet__head">
@@ -92,101 +87,119 @@ $sid = filter_input(INPUT_GET, 'club_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 											<i class="la la-gear"></i>
 										</span>
 										<h3 class="m-portlet__head-text">
-											<?php echo htmlspecialchars($club_name); ?>
+											<?php echo $club_name ?>
 										</h3>
 									</div>
 								</div>
+
 							</div>
 
-							<div class="m-portlet__body">
+					<div class="m-portlet__body">
 								<!--begin::Section-->
 								<div class="m-section">
 									<span class="m-section__sub">
                       <b>Advisor(s) </b><br>
-                      <?php senaraiPenasihat($club_id); ?>
-									</span>
-								</div>
-								<!-- tables stops here -->
-								<!--End::Section-->
-							</div>
-						</div>
+                      <?php senaraiPenasihat($club_id) ?>
+				</div>
+
+
+						<!-- tables stops here -->
+
+						<!--End::Section-->
 					</div>
+					<!--End::Row-->
+				</div>
 				</div>
 			</div>
 		</div>
 
     <div class="m-content">
       <div class="row">
-        <div class="col-lg-12">
-          <!--begin::Portlet-->
-          <div class="m-portlet" id="m_portlet">
-            <div class="m-portlet__head">
-              <div class="m-portlet__head-caption">
-                <div class="m-portlet__head-title">
-                  <span class="m-portlet__head-icon">
-                    <i class="flaticon-calendar-2"></i>
-                  </span>
-                  <h3 class="m-portlet__head-text">
-                    List of Registered Students
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div class="m-portlet__body">
-              <table class="table table-striped- table-bordered table-hover table-checkable" id="m_table_1">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Student No</th>
-                    <th>Name</th>
-                    <th>Program</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Total Marks</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  $sql_events = mysqli_query($connection, "SELECT * FROM club, club_registration, student WHERE club.club_id=club_registration.club_id AND student.stdNo=club_registration.stdNo AND club_registration.club_id='$sid'") or die(mysqli_error($connection));
-                  $z = 1;
+<div class="col-lg-12">
+<!--begin::Portlet-->
+<div class="m-portlet" id="m_portlet">
+<div class="m-portlet__head">
+  <div class="m-portlet__head-caption">
+    <div class="m-portlet__head-title">
+      <span class="m-portlet__head-icon">
+        <i class="flaticon-calendar-2"></i>
+      </span>
+      <h3 class="m-portlet__head-text">
+        List of Registered Students
+      </h3>
+    </div>
 
-                  while ($row = mysqli_fetch_array($sql_events)) {
-                    $reg_id = $row["reg_id"];
-                    $club_id = $row["club_id"];
-                    $club_name = $row["club_name"];
-                    $stdName = $row["stdName"];
-                    $stdNo = $row["stdNo"];
-                    $progCode = $row["progCode"];
-                    $noPhone = $row["noPhone"];
-                    $email = $row["email"];
-                  ?>
-                  <tr>
-                    <th scope="row"><?php echo $z; ?></th>
-                    <td><?php echo htmlspecialchars($stdNo); ?></td>
-                    <td><?php echo htmlspecialchars($stdName); ?></td>
-                    <td><?php echo htmlspecialchars($progCode); ?></td>
-                    <td><?php echo htmlspecialchars($email); ?></td>
-                    <td><?php echo htmlspecialchars($noPhone); ?></td>
-                    <td><?php echo sumMarks($stdNo); ?></td>
-                  </tr>
-                  <?php
-                    $z++;
-                  } // while loop ends properly
-                  ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <!--end::Portlet-->
-        </div>
-      </div>
+  </div>
+
+</div>
+<div class="m-portlet__body">
+  <table class="table table-striped- table-bordered table-hover table-checkable" id="m_table_1">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Student No</th>
+        <th>Name</th>
+        <th>Program</th>
+
+        <th>Email</th>
+        <th>Phone Number</th>
+        <th>Total Marks</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $sql_events = mysqli_query($connection, "select * from club,club_registration,student where  club.club_id=club_registration.club_id and student.stdNo=club_registration.stdNo and club_registration.club_id='$sid' ") or die (mysqli_error());
+      $z =1;
+
+      while ($row = mysqli_fetch_array($sql_events)) {
+        $reg_id = $row["reg_id"];
+        $club_id = $row["club_id"];
+        $club_name = $row["club_name"];
+        $stdName = $row["stdName"];
+        $stdNo = $row["stdNo"];
+        $progCode = $row["progCode"];
+        $noPhone = $row["noPhone"];
+        $email = $row["email"];
+
+      ?>
+      <tr>
+        <th scope="row"><?php echo $z ?></th>
+
+        <td><?php echo $stdNo ?></td>
+        <td><?php echo $stdName ?></td>
+        <td><?php echo $progCode ?></td>
+
+        <td><?php echo $email ?></td>
+        <td><?php echo $noPhone ?></td>
+        <td><?php echo sumMarks($stdNo) ?></td>
+
+      </tr>
+      <?php
+      $z++;
+    }
+    ?>
+
+
+    </tbody>
+  </table>
+
+</div>
+</div>
+<!--end::Portlet-->
+</div>
+</div>
+
       <!-- end of calendar -->
+
       <!-- start of widget -->
-      <!-- end of widget -->
+
+      <!-- end of widget -- >
+
       <!--End::Section-->
     </div>
+
+		</div>
 	</div>
-</div>
 			<!-- end:: Body -->
 <!-- begin::Footer -->
 		  <?php include("footer.php"); ?>
@@ -194,12 +207,14 @@ $sid = filter_input(INPUT_GET, 'club_id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 		</div>
 		<!-- end:: Page -->
     		        <!-- begin::Quick Sidebar -->
+
 		<!-- end::Quick Sidebar -->
 	    <!-- begin::Scroll Top -->
 		<div id="m_scroll_top" class="m-scroll-top">
 			<i class="la la-arrow-up"></i>
 		</div>
 		<!-- end::Scroll Top -->		    <!-- begin::Quick Nav -->
+
 		<!-- begin::Quick Nav -->
     	<!--begin::Base Scripts -->
 		<script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
