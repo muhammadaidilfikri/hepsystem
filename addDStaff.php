e4a0a8a9080bc8a821ce429649a62d6e97230c0a
Shoyu67 <shuhadasaadan05@gmail.com>
2025-12-16 14:37:56 +0800

    roleid 4 = HEA added

--- addDStaff.php
+++ addDStaff.php
@@ -11,7 +11,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 //roleid 1 = SUPER ADMINISTRATOR
 //roleid 2 = IT ADMINISTRATOR
 //roleid 3 = HEP
-$allowedroles = array(3); //roles allowed to access this page
+//roleid 4 = HEA
+$allowedroles = array(3,4); //roles allowed to access this page
 if (!in_array($_SESSION['roleid'], $allowedroles)) {
     header("Location: logout.php");
 }
